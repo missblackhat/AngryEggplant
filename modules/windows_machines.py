fd12ec134c5badab119438f0fed89c970ce4a135
colental <30509968+colental@users.noreply.github.com>
2017-11-15 06:27:00 -0800

    Update windows_machines.py

--- modules/windows_machines.py
+++ modules/windows_machines.py
@@ -4,16 +4,17 @@ import socket
 import win32com.client
 import win32net
 
-def machines_in_domain(domain_name):
-    adsi = win32com.client.Dispatch("ADsNameSpaces")
-    nt = adsi.GetObject("","WinNT:")
-    result = nt.OpenDSObject ("WinNT://%s" % domain_name, "", "", 0)
-    result.Filter = ["computer"]
-    for machine in result:
-        yield machine.Name
-
-domain_controller = win32net.NetGetDCName(None,None)
-domain_name = win32net.NetUserModalsGet(domain_controller, 2)['domain_name']
-print "Listing machines in", domain_name
-for machine in machines_in_domain(domain_name):
-    print machine
+def main():
+    try:
+        domain_controller = win32net.NetGetDCName(None,None)
+        domain_name = win32net.NetUserModalsGet(domain_controller, 2)['domain_name']
+        adsi = win32com.client.Dispatch("ADsNameSpaces")
+        nt = adsi.GetObject("","WinNT:")
+        result = nt.OpenDSObject ("WinNT://%s" % domain_name, "", "", 0)
+        result.Filter = ["computer"]
+        return [machine for machine in result]
+    except Exception as e:
+        return "Machine enumeration returned error: {}".format(str(e))
+    
+if __name__ == '__main__':
+    main()
