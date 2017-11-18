3cc49969423f37eb362b5e911e295c30fd0e71fe
colental <30509968+colental@users.noreply.github.com>
2017-11-17 16:40:24 -0800

    11-17-17

--- modules/windows_file_security.py
+++ modules/windows_file_security.py
@@ -3,50 +3,32 @@ import win32api
 import win32security
 import ntsecuritycon as con
 
-if len(sys.argv) < 2:
-  print 'usage: {} <filename>'.format(os.path.basename(sys.argv[0]))
-  sys.exit(0)
+FILENAME = sys.argv[1] if len(sys.argv) > 1 else None
 
-FILENAME = sys.argv[1]
-
-def show_cacls (filename):
-  print
-  print
-  for line in os.popen ("cacls %s" % filename).read ().splitlines ():
-      print line
+def show_cacls(filename):
+    if os.path.isfile(filename):
+        for line in os.popen("cacls %s" % filename).read().splitlines():
+            print line
 
 def main():
-  everyone, domain, type = win32security.LookupAccountName ("", "Everyone")
-  admins, domain, type = win32security.LookupAccountName ("", "Administrators")
-  user, domain, type = win32security.LookupAccountName ("", win32api.GetUserName ())
+    try:
+        everyone, domain, type = win32security.LookupAccountName("", "Everyone")
+        admins, domain, type = win32security.LookupAccountName("", "Administrators")
+        user, domain, type = win32security.LookupAccountName("", win32api.GetUserName ())
+        if os.path.isfile(FILENAME)
+        open(FILENAME, "w").close()
+        show_cacls(FILENAME)
+        sd = win32security.GetFileSecurity(FILENAME, win32security.DACL_SECURITY_INFORMATION)
+        dacl = win32security.ACL()
+        dacl.AddAccessAllowedAce(win32security.ACL_REVISION, con.FILE_GENERIC_READ, everyone)
+        dacl.AddAccessAllowedAce(win32security.ACL_REVISION, con.FILE_GENERIC_READ | con.FILE_GENERIC_WRITE, user)
+        dacl.AddAccessAllowedAce(win32security.ACL_REVISION, con.FILE_ALL_ACCESS, admins)
+        sd.SetSecurityDescriptorDacl(1, dacl, 0)
+        win32security.SetFileSecurity(FILENAME, win32security.DACL_SECURITY_INFORMATION, sd)
+        show_cacls(FILENAME)
+    except Exception as e:
+        print "Error: {}".format(str(e))
 
-  open(FILENAME, "w").close()
-  show_cacls (FILENAME)
-
-  #
-  # Find the DACL part of the Security Descriptor for the file
-  #
-  sd = win32security.GetFileSecurity (FILENAME, win32security.DACL_SECURITY_INFORMATION)
-
-  #
-  # Create a blank DACL and add the three ACEs we want
-  # We will completely replace the original DACL with
-  # this. Obviously you might want to alter the original
-  # instead.
-  #
-  dacl = win32security.ACL ()
-  dacl.AddAccessAllowedAce (win32security.ACL_REVISION, con.FILE_GENERIC_READ, everyone)
-  dacl.AddAccessAllowedAce (win32security.ACL_REVISION, con.FILE_GENERIC_READ | con.FILE_GENERIC_WRITE, user)
-  dacl.AddAccessAllowedAce (win32security.ACL_REVISION, con.FILE_ALL_ACCESS, admins)
-
-  #
-  # Put our new DACL into the Security Descriptor,
-  # update the file with the updated SD, and use
-  # CACLS to show what's what.
-  #
-  sd.SetSecurityDescriptorDacl (1, dacl, 0)
-  win32security.SetFileSecurity (FILENAME, win32security.DACL_SECURITY_INFORMATION, sd)
-  show_cacls (FILENAME)
 
 if __name__ == '__main__':
     main()
